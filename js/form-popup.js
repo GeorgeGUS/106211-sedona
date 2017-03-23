e05de9ee768c2312689056e2e53b424fa78d6e5e
GeorgeGUS <georgegus1@gmail.com>
2017-03-24 01:10:19 +0300

    Добавил открытие попапа при отправке формы, добавил ретину в блок интро

--- js/form-popup.js
+++ js/form-popup.js
@@ -1,21 +1,28 @@
 'use strict';
 var popup = document.querySelectorAll('.popup-overlay');
-var popupError = document.querySelector('.popup-overlay--error');
 var popupSuccess = document.querySelector('.popup-overlay--success');
+var form = document.querySelector('.form');
 
-var popupBtn = document.querySelectorAll('.popup__btn');
-var popupOkBtn = document.querySelector('.popup__ok-btn');
-var popupCloseBtn = document.querySelector('.popup__close-btn');
-
-
-/*__________ДОДЕЛАТЬ!____________*/
-for (var i=0; i < popupBtn.length; i++) {
-  popupBtn[i].onclick = function() {
-    if (!popup[i].classList.contains('popup-hide')) {
-      popupError.classList.add('popup-hide');
+var closePopup = function(evt) {
+  if (evt.target.classList.contains('popup__btn')) {
+    if (!this.classList.contains('popup-hide')) {
+      this.classList.add('popup-hide');
     }
-  };
+  }
+
+  if (evt.target.classList.contains('popup__send-btn')) {
+    form.submit();
+  }
+};
+
+var checkSubmit = function(evt) {
+  evt.preventDefault();
+  console.dir(evt.target);
+  popupSuccess.classList.remove('popup-hide');
+};
+
+for (var i = 0; i < popup.length; i++) {
+  popup[i].addEventListener('click', closePopup);
 }
 
-
-
+form.addEventListener('submit', checkSubmit);
